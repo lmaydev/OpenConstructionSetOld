33336275926fd00217d9d35b9ad1d638061b298c
Luke <allinighdrui@gmail.com>
2021-06-25 13:52:45 +0100

    Updated comments.

--- OpenConstructionSet/OcsHelper.cs
+++ OpenConstructionSet/OcsHelper.cs
@@ -16,14 +16,14 @@ namespace OpenConstructionSet
         /// <summary>
         /// Builds a <c>GameData</c> object by loading the given <c>mods</c> from the provided <c>folders</c>.
         /// </summary>
-        /// <param name="mods">Collection of mods to be loaded. Both names (e.g. example.mod) or full paths are accetped.</param>
+        /// <param name="mods">Collection of mods to be loaded. Both names (e.g. example.mod) and full paths are accetped.</param>
         /// <param name="folders">
         /// Collection of folders to search when resolving a mod's name to it's full path.
         /// All dependencies will be resolved this way.
         /// If <c>folders</c> is null the game folders will be used.
         /// </param>
-        /// <param name="activeMod">Name or full path of the mod to load as active. Both a name (e.g. example.mod) or a full path are accetped.</param>
-        /// <param name="resolveDependencies">If <c>true</c> dependencies will</param>
+        /// <param name="activeMod">Name or full path of the mod to load as active. Both a name (e.g. example.mod) and a full path are accetped.</param>
+        /// <param name="resolveDependencies">If <c>true</c> dependencies will will be resolved and mods loaded in order.</param>
         /// <returns>The built <c>GameData</c>.</returns>
         public static GameData Load(IEnumerable<string> mods, string activeMod = null, IEnumerable<GameFolder> folders = null, bool resolveDependencies = true)
         {
@@ -64,10 +64,10 @@ namespace OpenConstructionSet
         /// Initializes a new mod, saves it and then returns the mods full path.
         /// </summary>
         /// <param name="header">Contains the meta data for the mod.</param>
-        /// <param name="filename">Mod filename. e.g. example.mod</param>
+        /// <param name="mod">Mod filename. e.g. example.mod</param>
         /// <param name="folder">Folder to save mod in. If folder is <c>null</c> the game's mod folder will be used.</param>
         /// <returns>The full path of the mod.</returns>
-        public static string NewMod(Header header, string filename, GameFolder folder = null, bool deleteExisting = true)
+        public static string NewMod(Header header, string mod, GameFolder folder = null, bool deleteExisting = true)
         {
             if (folder == null)
             {
@@ -79,14 +79,14 @@ namespace OpenConstructionSet
                 folder = gameFolders.Mod;
             }
 
-            var path = folder.GetFullPath(filename);
+            var path = folder.GetFullPath(mod);
 
             if (!deleteExisting && System.IO.File.Exists(path))
             {
                 throw new Exception("Mod already exists");
             }
 
-            folder.Delete(filename);
+            folder.Delete(mod);
 
             var gameData = new GameData
             {
@@ -99,12 +99,12 @@ namespace OpenConstructionSet
         }
 
         /// <summary>
-        /// Search the provided folders to resolve a mod name (e.g. example.mod) to a full filename.
+        /// Search the provided folders to resolve a mod name (e.g. example.mod) to a full path.
         /// </summary>
         /// <param name="mod">The name of the mod file. e.g. example.mod.</param>
         /// <param name="folders">Collection of <see cref="GameFolder"/>s to search.</param>
-        /// <param name="path">If resolved this parameter will be set to the mod's full filename</param>
-        /// <returns>Returns <c>true</c> if the full filename was resolved</returns>
+        /// <param name="path">If resolved this parameter will be set to the mod's full path</param>
+        /// <returns>Returns <c>true</c> if the full path was resolved</returns>
         public static bool TryResolvePath(string mod, IEnumerable<GameFolder> folders, out string path)
         {
             if (System.IO.File.Exists(mod))
@@ -128,7 +128,15 @@ namespace OpenConstructionSet
             return false;
         }
 
-        public static IEnumerable<string> ResolvePaths(IEnumerable<string> mods, IEnumerable<GameFolder> folders)
+        /// <summary>
+        /// Search the provided folders to resolve the presented mods.
+        /// Mods that cannot be resolved will be dropped. Unless throwIfMissing is <c>true</c>.
+        /// </summary>
+        /// <param name="mods">Collections of the names of the mod files. e.g. example.mod.</param>
+        /// <param name="folders">Collection of <see cref="GameFolder"/>s to search.</param>
+        /// <param name="throwIfMissing">If <c>true</c> missing mods will result in an exception.</param>
+        /// <returns></returns>
+        public static IEnumerable<string> ResolvePaths(IEnumerable<string> mods, IEnumerable<GameFolder> folders, bool throwIfMissing = false)
         {
             var list = new List<string>();
 
@@ -138,6 +146,10 @@ namespace OpenConstructionSet
                 {
                     list.Add(path);
                 }
+                else if (throwIfMissing)
+                {
+                    throw new FileNotFoundException("could not resolve mode path", mod);
+                }
             }
 
             return list;
@@ -145,7 +157,7 @@ namespace OpenConstructionSet
 
         /// <summary>
         /// Resolve the depedencies of the provided mods and return a list of full filepaths of the mods and dependencies in load order.
-        /// The provided <see cref="GameFolder"/>s will be used to resolve mod names (example.mod) to full file paths. ALL dependencies will need to be resolved this way.
+        /// The provided <see cref="GameFolder"/>s will be used to resolve mod names (example.mod) to full file paths. All dependencies will need to be resolved this way.
         /// </summary>
         /// <param name="mods">Collection of mods names and/or full filenames.</param>
         /// <param name="folders">Collection of <see cref="GameFolder"/>s for use when resolving the full path and a mod name.</param>
