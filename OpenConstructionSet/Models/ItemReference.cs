2f18c03e5b1a8d5768c287fc38865da191b4461f
Luke <allinighdrui@gmail.com>
2021-06-28 22:17:16 +0100

    Added models and extension method to allow serialising of GameData Items

---
+++ OpenConstructionSet/Models/ItemReference.cs
@@ -0,0 +1,31 @@
+﻿using forgotten_construction_set;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenConstructionSet.Models
+{
+    public class ItemReference
+    {
+        public string Id { get; set; }
+
+        public ReferenceValues Values { get; set; }
+
+        public ItemReference()
+        {
+        }
+
+        public ItemReference(GameData.Reference reference)
+        {
+            if (reference is null)
+            {
+                throw new ArgumentNullException(nameof(reference));
+            }
+
+            Id = reference.itemID;
+            Values = new ReferenceValues(reference.Values);
+        }
+    }
+}
