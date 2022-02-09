2de08f770adb78c003b743bab2f28f3aa6a5722f
Luke May <luke@lmay.dev>
2022-02-09 21:31:41 +0000

    Separated collections into files.

---
+++ OpenConstructionSet/Mods/ModInstanceCollection.cs
@@ -0,0 +1,35 @@
+﻿namespace OpenConstructionSet.Mods;
+
+public class ModInstanceCollection : KeyedItemList<string, ModInstance>
+{
+    private readonly ModItem parent;
+
+    internal ModInstanceCollection(ModItem parent, IEnumerable<IInstance> collection) : this(parent)
+    {
+        foreach (var item in collection)
+        {
+            AddFrom(item);
+        }
+    }
+
+    internal ModInstanceCollection(ModItem parent)
+    {
+        this.parent = parent;
+    }
+
+    internal ModContext? Owner => parent.Owner;
+
+    public override void Add(ModInstance item)
+    {
+        item.SetParent(this);
+        base.Add(item);
+    }
+
+    public void Add(string id, string targetId, Vector3 position, Vector4 rotation, IEnumerable<string> states)
+        => Add(new(id, targetId, position, rotation, states));
+
+    public void Add(string id, IItem target, Vector3 position, Vector4 rotation, IEnumerable<string> states)
+        => Add(new(id, target.StringId, position, rotation, states));
+
+    public void AddFrom(IInstance instance) => Add(instance is ModInstance mi ? mi : new ModInstance(instance));
+}
---
+++ OpenConstructionSet/Mods/ModReferenceCategoryCollection.cs
@@ -0,0 +1,33 @@
+﻿using LMay.Collections;
+
+namespace OpenConstructionSet.Mods;
+
+public class ModReferenceCategoryCollection : SortedKeyedItemCollection<string, ModReferenceCategory>
+{
+    private readonly ModItem parent;
+
+    public ModReferenceCategoryCollection(ModItem parent, IEnumerable<IReferenceCategory> collection) : this(parent)
+    {
+        foreach (var item in collection)
+        {
+            AddFrom(item);
+        }
+    }
+
+    internal ModReferenceCategoryCollection(ModItem parent)
+    {
+        this.parent = parent;
+    }
+
+    internal ModContext? Owner => parent.Owner;
+
+    public override void Add(ModReferenceCategory item)
+    {
+        item.SetParent(this);
+        base.Add(item);
+    }
+
+    public void Add(string name) => Add(new ModReferenceCategory(name));
+
+    public void AddFrom(IReferenceCategory category) => Add(category is ModReferenceCategory mrc ? mrc : new ModReferenceCategory(category));
+}
